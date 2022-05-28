fcb621d814fc8ebc5c6d1213671ae9cd35de6f98
Artem Kliminskyi <graphene64@gmail.com>
2022-05-28 10:54:20 +0300

    Slightly optimize textured shader.

--- src/shaders/textured_fragment.glsl
+++ src/shaders/textured_fragment.glsl
@@ -1,5 +1,5 @@
 #version 330 core
-in vec3 vertex_normal_worldspace;
+in vec3 normal_worldspace;
 in vec3 fragment_pos_worldspace;
 in vec2 uv;
 
@@ -18,10 +18,7 @@ const int TX_DRW_POINT_LIGHTS_AMOUNT = 4;
 
 uniform PointLight POINT_LIGHTS[TX_DRW_POINT_LIGHTS_AMOUNT];
 uniform vec3 CAMERA_DIRECTION;
-uniform mat4 NORMAL_MATRIX;
 uniform sampler2D TEXTURE_SAMPLER;
-
-vec3 normal_worldspace = (NORMAL_MATRIX * vec4(vertex_normal_worldspace, 0.0)).xyz;
 
 const float AMBIENT_STRENGTH = 0.25;
 const vec3 AMBIENT_COLOR = vec3(1.0, 1.0, 1.0);
@@ -31,7 +28,9 @@ vec3 point_light(PointLight light) {
     light.diffuse_strength = 1.0;
     light.specular_strength = 0.5;
 
-    vec3 light_direction = normalize(fragment_pos_worldspace - light.position);
+    vec3 light_fragment_vec = fragment_pos_worldspace - light.position;
+    float d = length(light_fragment_vec);
+    vec3 light_direction = (fragment_pos_worldspace - light.position) / d;
     float diffuse = max(dot(normal_worldspace, -light_direction), 0.0);
     vec3 diffuse_color = light.diffuse_strength * diffuse * light.color;
 
@@ -39,7 +38,6 @@ vec3 point_light(PointLight light) {
     float specular = pow(max(dot(CAMERA_DIRECTION, reflect_dir), 0.0), SPECULAR_SHININESS);
     vec3 specular_color = light.specular_strength * specular * light.color;
 
-    float d = length(light.position - fragment_pos_worldspace);
     float attenuation = 1.0 / (light.const_coeff + light.linear_coeff * d + light.quadratic_coeff * (d * d));
 
     return (diffuse_color + specular_color) * attenuation;
--- src/shaders/textured_vertex.glsl
+++ src/shaders/textured_vertex.glsl
@@ -3,17 +3,20 @@ layout(location = 0) in vec3 vertex_pos_modelspace;
 layout(location = 1) in vec2 vertex_uv;
 layout(location = 2) in vec3 vertex_normal_modelspace;
 
-out vec3 vertex_normal_worldspace;
+out vec3 normal_worldspace;
 out vec3 fragment_pos_worldspace;
 out vec2 uv;
 
 uniform mat4 MVP;
 uniform mat4 MODEL_MATRIX;
+uniform mat4 NORMAL_MATRIX;
 
 void main() {
     gl_Position = MVP * MODEL_MATRIX * vec4(vertex_pos_modelspace, 1.0);
 
-    vertex_normal_worldspace = (MODEL_MATRIX * vec4(vertex_normal_modelspace, 0.0)).xyz;
+    vec3 orig_normal_worldspace = (MODEL_MATRIX * vec4(vertex_normal_modelspace, 0.0)).xyz;
+    normal_worldspace = (NORMAL_MATRIX * vec4(orig_normal_worldspace, 0.0)).xyz;
+
     fragment_pos_worldspace = (MODEL_MATRIX * vec4(vertex_pos_modelspace, 1.0)).xyz;
     uv = vertex_uv;
 }
