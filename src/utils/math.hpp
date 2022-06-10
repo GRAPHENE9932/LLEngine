846d58b161caa6ca3a7dd4f0ae1753633964d9ee
Artem Kliminskyi <graphene64@gmail.com>
2022-06-10 10:56:12 +0300

    Remove unused function from math.cpp.

--- src/utils/math.hpp
+++ src/utils/math.hpp
@@ -6,10 +6,5 @@
 #include "../structs/HorLine.hpp"
 
 namespace utils {
-    IntersectionCount vert_and_hor_line_intersection_points(
-        const VertLine& vert_line, const HorLine& hor_line,
-        glm::vec2& point_1
-    );
-
     Rect window_space_to_opengl_space(Rect rect, glm::vec2 window_size);
 }
