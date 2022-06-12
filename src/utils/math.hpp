f46c4744a0d95599d1bbb7d60c77b7b1debed3df
Artem Kliminskyi <graphene64@gmail.com>
2022-06-12 21:33:42 +0300

    Remove some unneeded includes.

--- src/utils/math.hpp
+++ src/utils/math.hpp
@@ -1,9 +1,6 @@
 #pragma once
 
-#include "../consts_and_enums.hpp"
 #include "../structs/Rect.hpp"
-#include "../structs/VertLS.hpp"
-#include "../structs/HorLS.hpp"
 
 namespace utils {
     Rect window_space_to_opengl_space(Rect rect, glm::vec2 window_size);
