aabfa248eaa63b5f0f85434a7f95d619c18882c0
Artem Kliminskyi <graphene64@gmail.com>
2022-06-16 20:11:24 +0300

    Add support for uncompressed textures.

--- src/common/Texture.cpp
+++ src/common/Texture.cpp
@@ -33,6 +33,7 @@ GLuint Texture::get_id() {
 constexpr uint32_t FOURCC_DXT1 = 0x31545844;
 constexpr uint32_t FOURCC_DXT3 = 0x33545844;
 constexpr uint32_t FOURCC_DXT5 = 0x35545844;
+constexpr uint32_t FOURCC_NO_COMPR = 0x00000000;
 
 struct DDSPixelFormat {
     uint32_t size;
@@ -85,6 +86,7 @@ void Texture::load_from_dds(std::string file_path) {
     // Handle the compression format.
     uint32_t block_size;
     GLint format;
+    bool is_compressed {true};
     switch (header.pixel_format.four_cc) {
         case FOURCC_DXT1:
             block_size = 8;
@@ -97,6 +99,11 @@ void Texture::load_from_dds(std::string file_path) {
         case FOURCC_DXT5:
             block_size = 16;
             format = GL_COMPRESSED_RGBA_S3TC_DXT5_EXT;
+            break;
+        case FOURCC_NO_COMPR:
+            block_size = 1;
+            format = GL_RGBA;
+            is_compressed = false;
             break;
         default:
             throw std::runtime_error(
@@ -116,13 +123,23 @@ void Texture::load_from_dds(std::string file_path) {
     uint32_t width = header.width;
     uint32_t height = header.height;
     for (GLint mipmap_level = 0;
-         mipmap_level < header.mip_map_count && width > 0 && height > 0;
-         mipmap_level++) {
-        uint32_t size = ((width + 3) / 4) * ((height + 3) / 4) * block_size;
+            mipmap_level < header.mip_map_count && width > 0 && height > 0;
+            mipmap_level++) {
+        uint32_t size;
+        if (is_compressed)
+            size = ((width + 3) / 4) * ((height + 3) / 4) * block_size;
+        else
+            size = width * height * 4;
+
         std::unique_ptr<char[]> buffer = std::make_unique<char[]>(size);
         stream.read(buffer.get(), size);
-        glCompressedTexImage2D(GL_TEXTURE_2D, mipmap_level, format, width, height,
-                               0, size, buffer.get());
+
+        if (is_compressed)
+            glCompressedTexImage2D(GL_TEXTURE_2D, mipmap_level, format, width, height,
+                    0, size, buffer.get());
+        else
+            glTexImage2D(GL_TEXTURE_2D, mipmap_level, GL_RGBA, width, height,
+                    0, format, GL_UNSIGNED_BYTE, buffer.get());
 
         width /= 2;
         height /= 2;
