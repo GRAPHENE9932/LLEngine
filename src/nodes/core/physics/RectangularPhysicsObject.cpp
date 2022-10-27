c74aecafb7feba4c1ab79920956ed6351d35d378
Artem Kliminskyi <graphene64@gmail.com>
2022-10-27 20:51:41 +0300

    Remove old physics code.

--- src/nodes/core/physics/RectangularPhysicsObject.cpp
+++
@@ -1,5 +0,0 @@
-#include "RectangularPhysicsObject.hpp"
-
-float RectangularPhysicsObject::horizontal_distance_from_cylinder(const Cylinder& cylinder) const {
-    return rect.distance_from({cylinder.position.x, cylinder.position.z}) - cylinder.radius;
-}
