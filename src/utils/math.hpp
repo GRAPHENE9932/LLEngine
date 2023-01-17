9b33748b1bb43095a6fa1159fa7664be440b9eb8
Artem Kliminskyi <graphene64@gmail.com>
2023-01-17 12:08:56 +0200

    Finish removing unneeded structs.

--- src/utils/math.hpp
+++ src/utils/math.hpp
@@ -2,8 +2,6 @@
 
 #include <glm/vec2.hpp>
 
-#include "structs/Rect.hpp"
-
 namespace utils {
     inline glm::vec2 scr_space_pos_to_gl_space(const glm::vec2 position, const glm::vec2 window_size) {
         return (position / window_size * 2.0f - 1.0f) * glm::vec2(1.0f, -1.0f);
