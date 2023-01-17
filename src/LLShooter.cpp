70a0a3fdb4413ee4a574e3bd12788f226a572fcb
Artem Kliminskyi <graphene64@gmail.com>
2023-01-17 13:18:39 +0200

    Remove a useless comment and minor formatting.

--- src/LLShooter.cpp
+++ src/LLShooter.cpp
@@ -8,8 +8,10 @@
 #include "common/core/Mesh.hpp"
 #include "LLShooter.hpp"
 #include <GLFW/glfw3.h>
+
 #include <memory>
 #include <iostream>
+
 const int WINDOW_WIDTH = 1500, WINDOW_HEIGHT = 800;
 
 LLShooter::~LLShooter() {
@@ -25,7 +27,6 @@ void LLShooter::init() {
     fps_meter = std::make_unique<FPSMeter>(0.25f);
 
     rendering_server = std::make_unique<RenderingServer>(glm::ivec2(WINDOW_WIDTH, WINDOW_HEIGHT));
-    //physics_server = std::make_unique<PhysicsServer>();
 
     Map map("res/maps/map_close.json");
     root_node = map.to_node(*rendering_server);
