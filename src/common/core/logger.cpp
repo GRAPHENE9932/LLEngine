007d75c825dde4968cded80b4b26d925d3721991
Artem Kliminskyi <graphene64@gmail.com>
2023-03-25 17:01:37 +0200

    Implement a logger.

---
+++ src/common/core/logger.cpp
@@ -0,0 +1,96 @@
+#include "logger.hpp"
+
+#include <fmt/format.h>
+#include <fmt/chrono.h>
+#include <fmt/ostream.h>
+
+#include <array>
+#include <mutex>
+#include <chrono>
+#include <atomic>
+#include <fstream>
+#include <iostream>
+
+std::atomic_bool console_logging_enabled {false};
+std::atomic<logger::Severity> minimum_console_log_severity {logger::INFO};
+
+std::unique_ptr<std::ofstream> log_file_stream {nullptr};
+std::atomic<logger::Severity> minimum_file_log_severity {logger::WARNING};
+
+std::mutex io_mutex;
+
+[[nodiscard]] std::string severity_to_string(logger::Severity severity) {
+    switch (severity) {
+    case logger::Severity::INFO:
+        return "INFO ";
+    case logger::Severity::WARNING:
+        return "WARNG";
+    case logger::Severity::ERROR:
+        return "ERROR";
+    case logger::Severity::FATAL_ERROR:
+        return "FATAL";
+    default:
+        return "UNKNW";
+    }
+}
+
+void logger::enable_console_logging() noexcept {
+    console_logging_enabled = true;
+}
+
+void logger::disable_console_logging() noexcept {
+    console_logging_enabled = false;
+}
+
+void logger::set_minimum_console_log_severity(Severity severity) noexcept {
+    minimum_console_log_severity = severity;
+}
+
+void logger::enable_file_logging(const std::string& path) noexcept {
+    std::scoped_lock lock { io_mutex };
+
+    if (log_file_stream) {
+        log_file_stream->close();
+        log_file_stream->open(path, std::ios::out | std::ios::app);
+    }
+    else {
+        log_file_stream = std::make_unique<std::ofstream>(path, std::ios::out | std::ios::app);
+    }
+}
+
+void logger::disable_file_logging() noexcept {
+    std::scoped_lock lock { io_mutex };
+
+    if (log_file_stream) {
+        log_file_stream->close();
+        log_file_stream = nullptr;
+    }
+}
+
+void logger::set_minimum_file_log_severity(Severity severity) noexcept {
+    minimum_file_log_severity = severity;
+}
+
+void logger::log(Severity severity, const std::string &message) noexcept {
+    const auto time = std::chrono::time_point_cast<std::chrono::microseconds>(std::chrono::system_clock::now());
+
+    std::string log_string {
+        fmt::format(
+            "{:%Y-%m-%d %H:%M:%S}.{:06} {} {}\n",
+            time,
+            time.time_since_epoch().count() % 1000000, // Microseconds.
+            severity_to_string(severity),
+            message
+        )
+    };
+
+    std::scoped_lock lock { io_mutex };
+
+    if (console_logging_enabled) {
+        std::cout << log_string;
+    }
+
+    if (log_file_stream) {
+        *log_file_stream << log_string;
+    }
+}
---
+++ src/common/core/logger.hpp
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <string>
+#include <cstdint>
+
+namespace logger {
+    enum Severity : std::uint8_t {
+        INFO = 0, WARNING = 1, ERROR = 2, FATAL_ERROR = 3
+    };
+
+    void enable_console_logging() noexcept;
+    void disable_console_logging() noexcept;
+    void set_minimum_console_log_severity(Severity severity) noexcept;
+
+    void enable_file_logging(const std::string& path) noexcept;
+    void disable_file_logging() noexcept;
+    void set_minimum_file_log_severity(Severity severity) noexcept;
+
+    void log(Severity severity, const std::string& message) noexcept;
+    inline void info(const std::string& message) noexcept { log(INFO, message); }
+    inline void warning(const std::string& message) noexcept { log(WARNING, message); }
+    inline void error(const std::string& message) noexcept { log(ERROR, message); }
+    inline void fatal_error(const std::string& message) noexcept { log(FATAL_ERROR, message); }
+}
