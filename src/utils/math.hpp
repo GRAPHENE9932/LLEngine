a730a3df631db9ab09b614427ce7fa40ed17f8f5
Artem Kliminskyi <graphene64@gmail.com>
2023-05-05 08:08:18 +0300

    Add new function overload in math_utils.
    Also, rename namespace utils to math_utils.

--- src/utils/math.hpp
+++ src/utils/math.hpp
@@ -1,10 +1,18 @@
 #pragma once
 
 #include <glm/vec2.hpp>
+#include <glm/vec3.hpp>
 
-namespace utils {
-    inline glm::vec2 scr_space_pos_to_gl_space(const glm::vec2 position, const glm::vec2 window_size) {
+namespace math_utils {
+    inline glm::vec2 scr_space_pos_to_gl_space(glm::vec2 position, glm::vec2 window_size) {
         return (position / window_size * 2.0f - 1.0f) * glm::vec2(1.0f, -1.0f);
+    }
+
+    inline glm::vec3 scr_space_pos_to_gl_space(const glm::vec3& position, glm::vec2 window_size) {
+        return {
+            scr_space_pos_to_gl_space(glm::vec2(position), window_size),
+            position.z
+        };
     }
 
     inline glm::vec2 scr_space_scale_to_gl_space(const glm::vec2 scale, const glm::vec2 window_size) {
