ae7a8ec5afa22ba6fcbb4bbd77792b995f4688ae
Artem Kliminskyi <graphene64@gmail.com>
2023-05-09 13:04:22 +0300

    Add custom node type registration.

---
+++ src/common/core/node_registration.cpp
@@ -0,0 +1,33 @@
+#include "node_registration.hpp"
+
+#include <map>
+
+std::map<std::string, CustomNodeType> custom_nodes_map;
+
+void add_node_factory(const std::string& node_type_name, CustomNodeFactory&& factory) {
+    custom_nodes_map[std::string(node_type_name)].factory = std::move(factory);
+}
+
+void add_custom_property(
+    const std::string& node_type_name, CustomPropertyType type,
+    const std::string& property_name, void(*abstract_setter_ptr)(SpatialNode*)
+) {
+    custom_nodes_map[node_type_name].properties.push_back({
+        type, property_name, abstract_setter_ptr
+    });
+}
+
+template<typename T>
+void register_custom_property(
+    const std::string& node_type_name,
+    const std::string& property_name,
+    void(*setter_ptr)(SpatialNode*, T)
+) {
+    if (!custom_nodes_map.contains(node_type_name)) {
+        throw std::runtime_error(
+            "Failed to register custom property because the custom node type is not registered yet."
+        );
+    }
+
+    custom_nodes_map[node_type_name].properties.push_back({get_custom_property_type<T>(), property_name});
+}
